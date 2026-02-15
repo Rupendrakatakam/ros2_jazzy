4e27d0f4023b13c432f4608f7af86cbad94dcbf6
katakamrupendra <katakamrupendra@gmail.com>
2026-02-15 21:41:46 +0530

    robot staet publiher

--- src/my_robot/launch/my_robot.launch.py
+++ src/my_robot/launch/my_robot.launch.py
@@ -1,49 +1,47 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch.substitutions import PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-import os
-import xacro
+
 
 
 def generate_launch_description():
-    pkg_myrobot = get_package_share_directory('my_robot')
-    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
-    # Process Xacro
-    xacro_file = os.path.join(pkg_myrobot, 'description', 'my_robot.urdf.xacro')
-    doc = xacro.process_file(xacro_file)
-    robot_desc = doc.toxml()
 
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_desc}]
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='my_robot' #<--- CHANGE ME
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        arguments=['-name', 'my_robot', '-string', robot_desc, '-z', '0.1']
-    )
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+             )
 
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock']
-    )
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_robot'],
+                        output='screen')
 
+
+
+    # Launch them all!
     return LaunchDescription([
-        # HEADLESS MODE (-s flag added!)
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
-            ),
-            launch_arguments={'gz_args': '-r empty.sdf'}.items(),
-        ),
-        robot_state_publisher,
+        rsp,
+        gazebo,
         spawn_entity,
-        bridge,
     ])
